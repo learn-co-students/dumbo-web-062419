6bafe6181b62a12bfe113daae2ef89c6cb82a9f2
Eric Kim <hyeokjungkim@gmail.com>
2019-07-19 13:47:18 -0400

    adds lecture

--- 16-rails-forms/app/controllers/unicorns_controller.rb
+++ 16-rails-forms/app/controllers/unicorns_controller.rb
@@ -1,4 +1,5 @@
 class UnicornsController < ApplicationController
+  before_action :find_unicorn, only: [:show, :edit, :update, :destroy]
 
   def index
     @unicorns = Unicorn.all
@@ -7,6 +8,41 @@ class UnicornsController < ApplicationController
 
   def show
     # byebug
+  end
+
+  def new
+    @unicorn = Unicorn.new
+  end
+
+  def create
+    @unicorn = Unicorn.create(unicorn_params)
+    redirect_to @unicorn
+    # redirect_to unicorn_path(@unicorn.id)
+    # redirect_to unicorn_path(@unicorn)
+    # redirect_to "/unicorns/#{@unicorn.id}"
+  end
+
+  def edit
+  end
+
+  def update
+    # byebug
+    @unicorn.update(unicorn_params)
+    redirect_to unicorn_path(@unicorn)
+  end
+
+  def destroy
+    @unicorn.destroy
+    redirect_to unicorns_path
+  end
+
+  private
+
+  def unicorn_params
+    params.require(:unicorn).permit(:name, :horn, :shoe)
+  end
+
+  def find_unicorn
     @unicorn = Unicorn.find(params[:id])
   end
 
--- 16-rails-forms/db/seeds.rb
+++ 16-rails-forms/db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+u1 = Unicorn.create(name: "Happicorn", shoe: 5, horn: 7)
+u1 = Unicorn.create(name: "Mr. Sparkle Face", shoe: 6, horn: 3)
+u1 = Unicorn.create(name: "Peter", shoe: 12, horn: 1)
+u1 = Unicorn.create(name: "Kissinger", shoe: 12, horn: 12)
+u1 = Unicorn.create(name: "Longhorn", shoe: 1, horn: 0)
