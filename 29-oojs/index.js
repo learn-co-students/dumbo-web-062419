f49f33da493293b65e31e008529ad60309d81fd5
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2019-08-15 10:21:51 -0400

    oo

---
+++ 29-oojs/index.js
@@ -0,0 +1,56 @@
+
+
+
+
+
+
+
+// const cup = {
+//   fullness: 0
+// }
+
+
+// function fill(cupToFill) {
+//   cupToFill.fullness = 10
+//   console.log(cup === cupToFill)
+// }
+
+// fill(cup)
+
+// console.log(cup)
+
+
+
+// const introduce = function(){
+//   return `Hey, I'm ${ this.fullName }`
+// }
+
+// const annie = {
+//   fullName: "Anne Souza",
+//   name: "Annie",
+//   introduce: introduce
+// }
+
+// const graham = {
+//   legalName: "Graham Aden Troyer-Joy",
+//   name: "Graham",
+//   introduce: introduce
+// }
+
+// const randy = {
+//   fullName: "Randy Herasme",
+//   name: "Randy",
+//   introduce: introduce
+// }
+
+// console.log(annie.introduce())
+// console.log(graham.introduce())
+// console.log(randy.introduce())
+
+// console.log(annie.introduce === graham.introduce)
+// console.log(annie.introduce() === graham.introduce())
+
+
+
+
+
